cdc3c8e06f27da92c56cfc418ab45cd36dff67ed
AndrzejWojtani <awojtania@9bits.com.pl>
2017-03-20 21:18:29 +0100

    log after exit to check

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,21 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	private static final Logger log = LogManager.getLogger();
+
     public int[] convertToInts(String[] strings) {
+    	log.debug("ConvertToInts entered [strings={}]", Arrays.toString(strings));
+    	String cos=Arrays.stream(strings).mapToInt(Integer::valueOf).toArray().toString();
+
+    	log.debug("ConvertToInts exiting",Arrays.toString(Arrays.stream(strings).mapToInt(Integer::valueOf).toArray()));
         return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    
     }
 }
